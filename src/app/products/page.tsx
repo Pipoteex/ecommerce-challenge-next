ca4b55d9be5788baaac465d33256d7d7e54386ec
Martin <telaf1997@gmail.com>
2023-09-07 00:28:16 -0300

    prueba en server side

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,23 +1,30 @@
-"use client"
+
 
 import React from 'react'
 import { useGetProductsQuery } from '@/redux/services/amiiboApi'
 import ProductComponent from '@/components/Product'
-import { Product } from '@/types/types'
+import { Product, Amiibo } from '@/types/types'
 
-const Products = () => {
+const getData = async () => {
+    let response = await fetch("https://www.amiiboapi.com/api/amiibo")
+    return response.json()
+}
 
-    const { data, isLoading, isFetching } = useGetProductsQuery(null)
+const Products = async () => {
+
+    /* const { data, isLoading, isFetching } = useGetProductsQuery(null)
 
     if (isLoading || isFetching) {
         return <div>Cargando..</div>
-    }
+    } */
+
+    let data: Amiibo = await getData()
 
     return (
 
         <div className='grid grid-cols-1 my-[20px] min-[580px]:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
             {
-                data?.amiibo.slice(0, 6).map((product: Product, index) => {
+                data?.amiibo?.slice(0, 6).map((product: Product, index) => {
                     return <ProductComponent key={index} data={product} />
                 })
             }
